4e6ffffe5ff91e0513b4bffaaf799184ae81a951
Tristan BAUER <tbauer@e3r10p3.42.fr>
2017-12-06 18:40:22 +0100

    ca avance

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbauer <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/06 17:18:49 by tbauer            #+#    #+#             */
+/*   Updated: 2017/12/06 18:39:39 by tbauer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include "libft.h"
+
+int	ft_error(char **str,char **line, int fd)
+{
+	if (fd == -1 || line == NULL)
+		return (-1);
+	if (!*str)
+	{
+		if (!(*str = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+			return (-1);
+	}
+	return (0);
+}
+
+char	*read_it(char *str, int fd)
+{
+	char	buf[BUFF_SIZE + 1];
+	int		ret;
+
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		str = ft_strjoin(str, buf);
+	}
+	return (str);
+}
 
 int	get_next_line(const int fd, char **line)
--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbauer <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/06 17:19:05 by tbauer            #+#    #+#             */
+/*   Updated: 2017/12/06 17:19:52 by tbauer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <sys/type.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define BUFF_SIZE 1000
