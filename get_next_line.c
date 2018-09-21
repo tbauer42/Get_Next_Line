69d7bf4b5c4efda19b279e86bf85072804eb80b7
Tristan Bauer <tristanbauer@MacBook-Pro-de-Tristan.local>
2018-09-21 14:46:18 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -1,76 +1,53 @@
-#include "get_next_line.h"
 #include "libft/includes/libft.h"
+#include "get_next_line.h"
+#include <stdio.h>
 
-static int	check_endline(char *str, size_t size)
+static int	make_line(int const fd, char *buffer, char *stock[fd])
 {
-	size_t i;
+	int		ret;
+	char	*temp;
 
-	i = 0;
-	while (i < size)
+	while ((ft_strchr(buffer, '\n') == NULL) &&
+			(ret = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
-		if (str[i] == '\0' || str[i] == '\n')
-			return (i);
-		i++;
+		buffer[ret] = '\0';
+		temp = stock[fd];
+		stock[fd] = ft_strjoin(temp, buffer);
+		ft_strdel(&temp);
 	}
-	return (-1);
-}
-
-static int	check_str(char *str, int i, char **line)
-{
-	int j;
-
-	if (str[i] == '\0')
-		j = i;
-	else
-		j = i + 1;
-	str[i] = '\0';
-	if (!(*line = ft_strdup(str)))
+	if (buffer)
+		ft_strdel(&buffer);
+	if (ret == -1)
 		return (-1);
-	str = ft_strcpy(str, str + j);
-	if (i == j)
-		str[0] = '\0';
 	return (1);
 }
 
-static int	found_endl(char **str, char *buff, int i, char **line)
+int			get_next_line(int const fd, char **line)
 {
-	if (!(*str = ft_strjoin_free(*str, buff, 1)))
+	static char	*stock[FD];
+	char		*buffer;
+	int			ret;
+	char		*str;
+	char		*tmp;
+
+	buffer = ft_strnew(BUFF_SIZE);
+	if (fd < 0 || line == NULL || buffer == NULL || BUFF_SIZE < 1)
 		return (-1);
-	i = check_endline(*str, ft_strlen(*str));
-	return (check_str(*str, i, line));
-}
-
-static int	read_loop(int fd, char **str, char *buff, char **line)
-{
-	int i;
-
-	while ((i = read(fd, buff, BUFF_SIZE)) > 0)
+	if (stock[fd] == NULL)
+		stock[fd] = ft_strnew(1);
+	if ((ret = make_line(fd, buffer, stock)) == -1)
+		return (-1);
+	printf("stok = %s\n", stock[fd]);
+	if ((str = ft_strchr(stock[fd], '\n')) != NULL)
 	{
-		buff[i] = '\0';
-		if ((i = check_endline(buff, i)) < 0)
-			*str = ft_strjoin_free(*str, buff, 1);
-		else
-			return (found_endl(str, buff, i, line));
+		printf("str = %s\n", str);
+		*line = ft_strsub(stock[fd], 0, str - stock[fd]);
+		tmp = stock[fd];
+		stock[fd] = ft_strdup(str + 1);
+		ft_strdel(&tmp);
+		return (1);
 	}
-	return (i);
-}
-
-int			get_next_line(const int fd, char **line)
-{
-	int			i;
-	static char	*str;
-	char		buff[BUFF_SIZE + 1];
-
-	if (str == NULL)
-		if (!(str = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
-			return (-1);
-	if ((i = check_endline(str, ft_strlen(str))) >= 0)
-		return (check_str(str, i, line));
-	if ((i = read_loop(fd, &str, buff, line)) > 0)
-		return (i);
-	if (str[0] != '\0')
-		return (check_str(str, ft_strlen(str), line));
-	if (i == 0)
-		return (0);
-	return (-1);
+	*line = ft_strdup(stock[fd]);
+	stock[fd] = NULL;
+	return (ft_strlen(*line) > 0 ? 1 : 0);
 }
